944191e0271eeca7d85cd2ee669fb4c02e3239de
alex frankel <afrankel3@luc.edu>
2024-09-25 18:41:38 -0500

    removed redundant/commented-out lines of code

--- src/serial.c
+++ src/serial.c
@@ -1,16 +1,13 @@
 #include <stdint.h>
 #include "serial.h"
 
-// Define the memory-mapped IO register address
+// Defines the MU_IO address
 #define MU_IO 0x3F215040
 
 // Function to print a single character to the serial port
 int putc(int data) {
     // Cast the MU_IO address to a volatile unsigned int pointer
-   // volatile unsigned int* mu_io = (volatile unsigned int*)MU_IO;
        *((volatile unsigned int*)MU_IO) = data;
     return data;
 
-    // Write the character to the MU_IO register
-   // *mu_io = (unsigned int)data;
 }
